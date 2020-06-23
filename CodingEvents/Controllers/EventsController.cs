b8e47024626b5405b183973a153ada6cae66d8bf
Mitch Hiltz <mitch.hiltz@gmail.com>
2020-06-23 12:56:54 -0400

    Prior to exercises

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private List<string> Events = new List<string>();
+
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.events = Events;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Add")]
+        public IActionResult NewEvent(string name)
+        {
+            Events.Add(name);
+
+            return Redirect("/Events");
+        }
+    }
+}
