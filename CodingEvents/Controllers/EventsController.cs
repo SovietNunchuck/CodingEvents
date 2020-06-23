a35bfb2aa94753c3a9943533cd2cc9dd0e63526c
Mitch Hiltz <mitch.hiltz@gmail.com>
2020-06-23 14:09:48 -0400

    Couldn't figure out how to increment first column

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -8,7 +8,7 @@ namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
-        static private List<string> Events = new List<string>();
+        static private Dictionary<string, string> Events = new Dictionary<string, string>();
 
         // GET: /<controller>/
         [HttpGet]
@@ -27,9 +27,9 @@ namespace CodingEvents.Controllers
 
         [HttpPost]
         [Route("/Events/Add")]
-        public IActionResult NewEvent(string name)
+        public IActionResult NewEvent(string name, string description)
         {
-            Events.Add(name);
+            Events.Add(name, description);
 
             return Redirect("/Events");
         }
