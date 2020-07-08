31ffba2387b450e91c3b2b1b5493e5dedf1cb3c9
Mitch Hiltz <mitch.hiltz@gmail.com>
2020-07-08 10:41:15 -0400

    Exercises complete

---
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodingEvents.Data;
+using CodingEvents.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext context;
+
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<EventCategory> categories = context.Categories.ToList();
+
+            return View(categories);
+        }
+    }
+}
