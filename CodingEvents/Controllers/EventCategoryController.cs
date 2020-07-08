a5e4c870c2f3ee0479c8061ffc9be6438b1cb5a1
Mitch Hiltz <mitch.hiltz@gmail.com>
2020-07-08 11:51:46 -0400

    Studio complete

--- CodingEvents/Controllers/EventCategoryController.cs
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CodingEvents.Data;
 using CodingEvents.Models;
+using CodingEvents.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodingEvents.Controllers
@@ -23,6 +24,31 @@ namespace CodingEvents.Controllers
             List<EventCategory> categories = context.Categories.ToList();
 
             return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+            return View(addEventCategoryViewModel);
+        }
+
+        [HttpPost("/EventCategory/Create")] //TODO: Choose route
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory newEventCategory = new EventCategory
+                {
+                    Name = addEventCategoryViewModel.Name
+                };
+
+                context.Categories.Add(newEventCategory);
+                context.SaveChanges();
+
+                return Redirect("/EventCategory");
+            }
+            return View("Create", addEventCategoryViewModel);
         }
     }
 }
---
+++ CodingEvents/ViewModels/AddEventCategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodingEvents.ViewModels
+{
+    public class AddEventCategoryViewModel
+    {
+        [Required(ErrorMessage = "Required field")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Category name must be between 3 and 20 characters long.")]
+        public string Name { get; set; }
+    }
+}
